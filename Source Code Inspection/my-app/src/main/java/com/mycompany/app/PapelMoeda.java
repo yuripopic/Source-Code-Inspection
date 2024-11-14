4290716c3b3e06d6203653b174c42cb993118d14
Lucas Forge Freo <88105038+forgelucas@users.noreply.github.com>
2024-11-14 19:58:54 -0300

    Teste JUnit implementados

---
+++ Source Code Inspection/my-app/src/main/java/com/mycompany/app/PapelMoeda.java
@@ -0,0 +1,24 @@
+package com.mycompany.app;
+
+/**
+ *
+ * @author Calebe de Paula Bianchini
+ */
+public class PapelMoeda {
+
+    protected int valor;
+    protected int quantidade;
+
+    public PapelMoeda(int valor, int quantidade) {
+        this.valor = valor;
+        this.quantidade = quantidade;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+}
