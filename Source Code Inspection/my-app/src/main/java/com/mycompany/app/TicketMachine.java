6a61159e7b881d6fffb2e7648113d283467e0a34
Lucas Forge Freo <88105038+forgelucas@users.noreply.github.com>
2024-11-15 13:31:23 -0300

    exceptions corrigidas

--- Source Code Inspection/my-app/src/main/java/com/mycompany/app/TicketMachine.java
+++ Source Code Inspection/my-app/src/main/java/com/mycompany/app/TicketMachine.java
@@ -3,44 +3,71 @@ package com.mycompany.app;
 import java.util.Iterator;
 
 /**
- *
- * @author Calebe de Paula Bianchini
+ * Classe TicketMachine para gerenciamento de tickets e saldo.
  */
 public class TicketMachine {
 
-    protected int valor;
-    protected Troco troco;
-    protected int saldo;
-    protected int[] papelMoeda = {2, 5, 10, 20, 50, 100};
+    protected int valor; // Valor do ticket
+    protected Troco troco; // Classe para cálculo de troco
+    protected int saldo; // Saldo atual inserido na máquina
+    protected int[] papelMoeda = {2, 5, 10, 20, 50, 100}; // Valores válidos de notas/moedas
 
+    // Construtor da TicketMachine
     public TicketMachine(int valor) {
         this.valor = valor;
         this.saldo = 0;
         this.troco = new Troco(valor);
     }
 
-    public void inserir(int quantia){
+    /**
+     * Insere uma quantia de dinheiro na máquina.
+     *
+     * @param quantia Valor a ser inserido
+     * @throws PapelMoedaInvalidaException Caso a quantia seja inválida
+     */
+    public void inserir(int quantia) throws PapelMoedaInvalidaException {
         boolean achou = false;
-        for (int i = 0; i < papelMoeda.length && !achou; i++) {
-            if (papelMoeda[1] == quantia) {
+        for (int i = 0; i < papelMoeda.length; i++) {
+            if (papelMoeda[i] == quantia) {
                 achou = true;
+                break;
             }
         }
         if (!achou) {
-            System.out.println("Moeda invalida!");
+            throw new PapelMoedaInvalidaException(); // Lança exceção com mensagem padrão
         }
         this.saldo += quantia;
     }
-    
+
+    /**
+     * Retorna o saldo atual.
+     *
+     * @return Saldo atual inserido
+     */
     public int getSaldo() {
         return saldo;
     }
 
+    /**
+     * Retorna um iterador para o troco.
+     *
+     * @return Iterador do troco
+     */
     public Iterator<PapelMoeda> getTroco() {
         return troco.getIterator();
     }
 
-    public String imprimir() {
+    /**
+     * Imprime o ticket, reduzindo o saldo pelo valor do ticket.
+     *
+     * @return String representando o ticket
+     * @throws SaldoInsuficienteException Caso o saldo seja insuficiente
+     */
+    public String imprimir() throws SaldoInsuficienteException {
+        if (saldo < valor) {
+            throw new SaldoInsuficienteException(); // Lança exceção com mensagem padrão
+        }
+        saldo -= valor; // Reduz o saldo pelo valor do ticket
         String result = "*****************\n";
         result += "*** R$ " + valor + ",00 ****\n";
         result += "*****************\n";
